7557eb879771c05962d0d67074c732db65274c5a
Emrys <emrys@openenergymonitor.org>
2020-03-25 21:27:03 +0000

    added build-essentials and rpi.gpio to emonhub

--- emonhub/connected.py
+++ emonhub/connected.py
@@ -7,7 +7,8 @@ GPIO.setwarnings(False)
 button = 22 
 
 GPIO.setup(button, GPIO.IN, GPIO.PUD_DOWN)
-
+print("Short BCM %s to HIGH 3.3V to trigger action...", button)
+print("@see pinout command or https://pinout.xyz for layout")
 while True:
     button_state = GPIO.input(button)
     if button_state == GPIO.HIGH:
