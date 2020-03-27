1aba784eb13b9e12dc8a32450cff7f90f4a787d8
Emrys Roberts <emrys@openenergymonitor.org>
2020-03-27 00:26:13 +0000

    added device module,first attempt at reboot

--- emonhub/entrypoint.sh
+++ emonhub/entrypoint.sh
@@ -2,6 +2,15 @@
 # Disable bluetooth device and restores UART0/ttyAMA0
 # once the device tree overlay setting is added to the /boot/config.txt the host must reboot
 
+if [ -z "$REBOOT_TOGGLE" ]; then
+  cat >&2 <<EOF
+A REBOOT_TOGGLE is required to run this container.
+$(printenv|sort)
+EOF
+  exit 1
+fi
+exec "$@"
+
 set -e
 
 if [ -e ${REBOOT_TOGGLE} ]
