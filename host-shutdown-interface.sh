1a9a84b95dc5de694926164bc81c2c21fc7651dd
Emrys <emrys@openenergymonitor.org>
2020-03-30 16:58:13 +0100

    added messages to user in host-shut-down-interface.sh

--- host-shutdown-interface.sh
+++ host-shutdown-interface.sh
@@ -13,12 +13,15 @@ while inotifywait -e close_write $SIGNAL_FILE; do
   signal=$(cat $SIGNAL_FILE)
   if [ "$signal" == "shutdown" ]; then 
     sudo echo "shutdown done" > file
+    echo "----------shutting down..."
     sudo shutdown -h now
   elif [ "$signal" == "reboot" ]; then
     sudo echo "reboot done" > file
+    echo "----------rebooting..."
     sudo shutdown -r now
   elif [ "$signal" == "restart-emonhub" ]; then
     sudo echo "restarted emonhub" > file
+    echo "----------restarting emonhub..."
     docker-compose restart emonhub
   fi
 done
